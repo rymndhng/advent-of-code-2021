133a173d117a22cff14f82e68e3a9cc98a1b2d62
Raymond Huang <rayh@hey.com>
2021-12-05 17:14:40 -0800

    finish day 1

--- src/day01.rs
+++ src/day01.rs
@@ -3,15 +3,10 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-pub fn main() -> std::io::Result<()> {
-    let file = File::open("input/001.txt")?;
-    let reader = BufReader::new(file);
-
-    let mut numbers = reader.lines().map(|x| x.unwrap().parse::<i32>().unwrap());
-
+fn part1(numbers: &Vec<i32>) {
+    let mut numbers = numbers.iter();
     // first number
     let init = numbers.next().unwrap();
-
     let state = numbers.fold((0, init), |state, n| {
         let (count, last_n) = state;
 
@@ -22,7 +17,36 @@ pub fn main() -> std::io::Result<()> {
 
         (count, n)
     });
+    println!("Part 1: count: {:?}, elements: {:?}", state.0, state.1);
+}
 
-    println!("count: {:?}, elements: {:?}", state.0, state.1);
+fn part2(numbers: &Vec<i32>) {
+    let mut numbers = numbers.windows(3).map(|nums| nums.iter().sum::<i32>());
+
+    let init = numbers.next().unwrap();
+    let state = numbers.fold((0, init), |state, n| {
+        let (count, last_n) = state;
+
+        let count = match n.cmp(&last_n) {
+            Ordering::Greater => count + 1,
+            _ => count,
+        };
+
+        (count, n)
+    });
+    println!("Part 2: count: {:?}, elements: {:?}", state.0, state.1);
+}
+
+pub fn main() -> std::io::Result<()> {
+    let file = File::open("input/001.txt")?;
+    let reader = BufReader::new(file);
+
+    let numbers = reader
+        .lines()
+        .map(|x| x.unwrap().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    part1(&numbers);
+    part2(&numbers);
     Ok(())
 }
